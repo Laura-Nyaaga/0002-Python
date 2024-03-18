7df713d5306e81336316b6dbf4fac88f50f83c7b
Laura-Nyaaga <nyaagaaluoch@gmail.com>
2024-03-18 22:50:24 +0300

    add files

---
+++ class.py
@@ -0,0 +1,44 @@
+a = [1, 2, 3]
+b = ['a', 'b', 'c']
+c = [1, 2, 'a', 'b', 'c', True, False]
+print(type(a))
+print(type(b))
+print(type(c))
+# Data structures: List
+d = a+b
+print(d)
+e = a*4
+print(e)
+fruits = ['Orange', 'Banana', 'Apple', 'Mango', 'Passion']
+print(fruits)
+print(fruits.append('Melon'))
+print(fruits.extend(['Grapes','Avacado']))
+print(fruits)
+print(fruits.insert(0, 'Pineapples'))
+print(fruits)
+print(fruits.insert(-1, 'Kiwi'))
+print(fruits)
+print(fruits.sort())
+print(fruits)
+print(fruits.reverse())
+print(fruits)
+print(fruits.remove('Melon'))
+print(fruits)
+# print(fruits.remove('cat'))
+# print(fruits)
+print(fruits.pop())
+print(fruits)
+# print(del fruits[3])
+# print(fruits)
+# print(len.fruits())
+# print(fruits)
+print(fruits.clear())
+print(fruits)
+
+# Lists indexing and Slicing:
+cart = ['sugar', 'salt' ,'Bread', 'Tea', 'Milk']
+print(cart[-4])
+print(cart[2])
+print(cart[:3])
+print(cart[-4:4])
+print(cart[1:-2])
